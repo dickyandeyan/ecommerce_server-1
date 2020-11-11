4d49cc7a8a0debdc3edc891456edb5af4c5fe3bc
dickyandeyan <dickyundead@gmail.com>
2020-11-11 23:17:13 +0700

    add get product change environtment to develeopment

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,17 @@
 const { Product } = require('../models/index')
 
 class ProductController {
+  static async getProduct(req, res, next) {
+    try {
+      const product = await Product.findAll({
+        order: [['id', 'DESC']]
+      })
+      res.status(200).json(product)
+    } catch (error) {
+      next(error)
+    }
+  }
+
   static async addProduct(req, res, next) {
     const userId = +req.loggedInUser.id
     try {
@@ -43,7 +54,7 @@ class ProductController {
       )
 
       if (updated[0] > 0) {
-        res.status(200).json({ message: 'Product updated!' })
+        res.status(200).json({ message: 'Product updated!'})
       } else {
         next()
       }
--- routes/product.js
+++ routes/product.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const ProductController = require('../controllers/productController')
 const authorization = require('../middlewares/authorization')
 
+router.get('/product', ProductController.getProduct)
 router.post('/product', authorization, ProductController.addProduct)
 router.put('/product/:id', authorization, ProductController.updateProduct)
 router.delete('/product/:id', authorization, ProductController.deleteProduct)
